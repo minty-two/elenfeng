99af82c05d96e2730c201fb40e2ec12d73cfb212
marc pickering <marc.pickering@two-uk.com>
2020-05-30 17:28:45 +0100

    Updates v1

--- app/js/portfolio.js
+++ app/js/portfolio.js
@@ -1,8 +1,10 @@
 var menswearSlides = 44;
 var womenswearSlides = 17;
+var mydreamSlides = 6;
 $( document ).ready(function() {
 
-	var pageIdentifier = $('#portfolio').attr('page-id');
+	var slideContainer = '.main-slider .slide-container';
+	var pageIdentifier = $('.slider-page').attr('page-id');
 	var pageArr;
 	var $loadingTime = 3000;
 
@@ -11,6 +13,8 @@ $( document ).ready(function() {
 		pageArr = menswearSlides;
 	} else if ( pageIdentifier == 'womenswear' ) {
 		pageArr = womenswearSlides;
+	} else if ( pageIdentifier == 'mydream' ) {
+		pageArr = mydreamSlides;
 	}
 
 	$.fakeLoader({
@@ -19,84 +23,33 @@ $( document ).ready(function() {
         spinner:'spinner7'
     });
 
-	newFunction('.main-slider',pageArr, pageIdentifier)
-	newFunction('.thumb-slider',pageArr, pageIdentifier)
+	newFunction(slideContainer,pageArr, pageIdentifier)
 
-	setTimeout(function(){ 
-		$('.main-slider').slick({
-	  slidesToShow: 1,
-	  slidesToScroll: 1,
-	  arrows: false,
-	  fade: true,
-	  asNavFor: '.thumb-slider',
-	  adaptiveHeight: true,
-	});
-	$('.thumb-slider').slick({
-	  slidesToShow: 5,
-	  slidesToScroll: 1,
-	  infinite: false,
-	  asNavFor: '.main-slider',
-	  dots: false,
-	  centerMode: false,
-	  focusOnSelect: true,
-	  prevArrow: $('.prev'),
-	  nextArrow: $('.next'),
-	  adaptiveHeight: true,
-	  responsive: [{
-	    breakpoint: 1198,
-	    settings: {
-	        slidesToShow: 3,
-	        // centerMode: false,
-	      }
-	    },]
-	});
-	},  $loadingTime);
 	// SLICK
- //   $('.main-slider').slick({
-	//   slidesToShow: 1,
-	//   slidesToScroll: 1,
-	//   arrows: false,
-	//   fade: true,
-	//   asNavFor: '.thumb-slider',
-	//   adaptiveHeight: true,
-	// });
-	// $('.thumb-slider').slick({
-	//   slidesToShow: 5,
-	//   slidesToScroll: 1,
-	//   infinite: false,
-	//   asNavFor: '.main-slider',
-	//   dots: false,
-	//   centerMode: false,
-	//   focusOnSelect: true,
-	//   prevArrow: $('.prev'),
-	//   nextArrow: $('.next'),
-	//   responsive: [{
-	//     breakpoint: 1198,
-	//     settings: {
-	//         slidesToShow: 3,
-	//         // centerMode: false,
-	//       }
-	//     },]
-	// });
-	// END SLICK
-	
-	// applySlideImgs('.main-slider .slick-slide');
-	// applySlideImgs('.thumb-slider .slick-slide');
-	
-	function applySlideImgs(sel){
-		$(sel).each(function(index, el) {
-			index++;
-			var $img = '../img/portfolio/can-you-see-me-now/menswear/can_you_see_me_now_menswear_page-'+index+'.jpg'
-			$(this).append('<img src="'+$img+'" style="visibility: hidden;" />')
-			$(this).css('background-image', 'url("'+$img+'")');
+	setTimeout(function(){ 
+		$(slideContainer).slick({
+	  	slidesToShow: 1,
+	  	slidesToScroll: 1,
+	  	dots: true,
+	  	arrows: true,
+	  	prevArrow: $('.prev'),
+	  	nextArrow: $('.next'),
+	  	fade: true,
+	  	adaptiveHeight: true,
 		});
-	}
+	},  $loadingTime);
+	// END SLICK
 
 	function newFunction(sel,arrInt, folder){
-		console.log(arrInt)
 		var totalSlides = arrInt;
-		for (var i = 1; i < totalSlides; i++) {
-			$(sel).append('<div data-slide-id="'+i+'"><img src="../img/portfolio/can-you-see-me-now/'+folder+'/'+i+'.jpg"/></div>')
+		if ( pageIdentifier == 'mydream' ) {
+			for (var i = 1; i < totalSlides; i++) {
+				$(sel).append('<div data-slide-id="'+i+'"><img src="../img/gallery/'+folder+'/'+i+'.jpg"/></div>')
+			}
+		} else {
+			for (var i = 1; i < totalSlides; i++) {
+				$(sel).append('<div data-slide-id="'+i+'"><img src="../img/portfolio/can-you-see-me-now/'+folder+'/'+i+'.jpg"/></div>')
+			}
 		}
 	}
 
